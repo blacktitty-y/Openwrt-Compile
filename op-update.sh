84ce1381baeb87ef1491617c02572a20db0e7661
Blacktitty <52312781+blacktitty-y@users.noreply.github.com>
2021-04-24 21:36:58 +0800

    Update op-update.sh

--- op-update.sh
+++ op-update.sh
@@ -2,6 +2,11 @@
 
 cd lede
 
+#删除拉取插件
+cd package/lean
+rm -rf luci-theme-argon luci-app-argon-config luci-app-serverchan luci-app-adguardhome
+cd -
+
 # 放弃本地修改 使远程库内容强制覆盖本地代码
 git fetch --all && git reset --hard
 
@@ -12,13 +17,8 @@ sed -i 's/"blacktitty"/"root"/g' feeds/luci/modules/luci-mod-admin-full/luasrc/c
 sed -i 's/"blacktitty"/"root"/g' feeds/luci/modules/luci-base/luasrc/controller/admin/servicectl.lua
 sed -i 's/user blacktitty root/user root/g' feeds/packages/net/nginx-util/files/uci.conf.template
 
-# 升级
-git pull && cd package/lean
-rm -rf luci-theme-argon luci-app-argon-config luci-app-serverchan luci-app-adguardhome
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git && git clone https://github.com/jerrykuku/luci-app-argon-config.git && git clone https://github.com/tty228/luci-app-serverchan.git && git clone https://github.com/rufengsuixing/luci-app-adguardhome.git && cd -
-
-# 更新
-./scripts/feeds update -a && ./scripts/feeds install -a
+# 升级更新
+git pull && ./scripts/feeds update -a && ./scripts/feeds install -a
 
 echo "
 首次用:
